7928008073f10f267474126b256092ee129ca5e8
bala510 <85919797+bala510@users.noreply.github.com>
2021-06-16 18:51:02 +0530

    Add files via upload

---
+++ factorial of a number.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class facto {
+
+	public static void main(String[] args) {
+		Scanner obj = new Scanner(System.in);
+		System.out.println("enter number");
+		int n=obj.nextInt();
+		int f =1;
+		for(int i=1; i<=n;i++)
+		{
+			f=f*i;
+		}
+				System.out.println(" factorial= " +f);
+
+
+	}
+
+}
+
